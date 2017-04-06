7df130131338fb1348984770169dfba9ce7f7b3e
doubleface <christophe@thiriot.xyz>
2017-04-06 16:17:45 +0200

    Fixed token file exist problem

--- data/env.js
+++ data/env.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 module.exports = {
-  COZY_CREDENTIALS: fs.readFileSync(path.join(__dirname, 'token.json')),
+  COZY_CREDENTIALS: fs.existsSync(tokenPath) ? fs.readFileSync(tokenPath) : 'NO TOKEN',
   COZY_URL: 'http://cozy.tools:8080',
   COZY_FIELDS: '{"login": "login", "password": "password", "folderPath": "folderPath"}'
 }
