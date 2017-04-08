2f3322df6f4f6cd1e0400f93a998a3821d04ff85
doubleface <christophe@thiriot.xyz>
2017-04-08 08:56:43 +0200

    Fixed data/env.js with missing line

--- data/env.js
+++ data/env.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const tokenPath = path.join(__dirname, 'token.json')
 module.exports = {
   COZY_CREDENTIALS: fs.existsSync(tokenPath) ? fs.readFileSync(tokenPath) : 'NO TOKEN',
   COZY_URL: 'http://cozy.tools:8080',
