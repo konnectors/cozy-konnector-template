d70e6d0e17169d3f1d1fe9c9ba361805c22f29aa
doubleface <christophe@thiriot.xyz>
2017-06-21 16:41:35 +0200

    WIP : adapation to cozy-konnector-libs v3

--- data/env_standalone.js
+++
@@ -1,5 +0,0 @@
-module.exports = Object.assign(require('./env.js'), {
-  NODE_ENV: 'standalone',
-  COZY_FIELDS: `{"connector": "mykonnector", "account": "noid", "folder_to_save": "folderPath"}`,
-  DEBUG: '*'
-})
