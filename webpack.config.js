0bf233aedf7dea1aa6aca86ef1ee47c054fcfea1
doubleface <christophe@cozycloud.cc>
2018-03-09 19:28:10 +0100

    fix webpack config file to output index.js file

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   target: 'node',
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'konnector.js'
+    filename: 'index.js'
   },
   plugins: [
     new CopyPlugin([
