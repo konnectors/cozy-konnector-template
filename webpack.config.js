9459d761d06b71bdb463cb645ead25cb565bc822
doubleface <christophe@cozycloud.cc>
2018-04-17 14:57:30 +0200

    Publish to the registry with travis

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
       { from: 'manifest.konnector' },
       { from: 'package.json' },
       { from: 'README.md' },
+      { from: 'assets' },
       { from: 'LICENSE' }
     ])
   ]
