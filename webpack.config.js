c1bba13fdee87e4e716b2281c72599a50a74994b
doubleface <christophe@cozycloud.cc>
2019-03-13 11:21:15 +0100

    fix: remove Pouchdb as external dep since it is not supported by cozy-client-js

--- webpack.config.js
+++ webpack.config.js
@@ -54,16 +54,7 @@ module.exports = {
     // Critical dependency: the request of a dependency is an expression
     // Since we cannot change this dependency. I think it won't hide more important messages
     exprContextCritical: false
-  },
-  externals: [
-    {
-      // pouchdb comes from cozy-client-js and is not used by the connectors
-      pouchdb: 'PouchDB'
-      // If you want to make the built version of the connector lighter and you do not read pdf
-      // files content
-      // 'pdfjs-dist': 'pdfjs'
-    }
-  ]
+  }
 }
 
 function optimizeSVGIcon(buffer, path) {
