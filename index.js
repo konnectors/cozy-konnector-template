0d3a56cb40e989504dfb0b8cca76891eb6d1efab
doubleface <christophe@cozycloud.cc>
2020-03-10 15:57:22 +0100

    publish: chore: upgrade dependencies
    generated from commit 9d0deb6e77c53f898e7691665dea0ab10a94cc3e

--- index.js
+++ index.js
@@ -91,7 +91,7 @@
 function logMemoryUsage(label) {
   const used = process.memoryUsage().heapUsed / 1024 / 1024
   console.log(`Memory usage ${label} : ${Math.round(used * 100) / 100} MB`)
-  if (Math.round(used * 100) / 100 > 200) {
+  if (Math.round(used * 100) / 100 > 300) {
     console.log('finished')
     process.exit(0)
   }
