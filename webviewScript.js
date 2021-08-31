94d4f907e0dbb56e5a60480aa778610460f34bce
doubleface <christophe@cozycloud.cc>
2021-08-31 16:59:45 +0200

    publish: init
    generated from commit 38b91a2e8beeed856358aa83e50ea4dc60b3f8e0

--- webviewScript.js
+++ webviewScript.js
@@ -5,9 +5,10 @@ module.exports = {
   "name": "Template",
   "type": "konnector",
   "language": "node",
+  "clientSide": "true",
   "icon": "icon.svg",
   "slug": "template",
-  "source": "git@github.com:konnectors/template.git",
+  "source": "git@github.com:konnectors/cozy-konnector-template.git",
   "editor": "Cozy",
   "vendor_link": "http://books.toscrape.com/",
   "categories": [
